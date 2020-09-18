1d60bd26d55d48618c0d567c7171786b9baf3bcd
Ashwath V S <ashsat.vijayan122@gmail.com>
2020-09-18 18:00:04 +0100

    Update longest_common_prefix.cpp

--- leetcode/longest_common_prefix.cpp
+++ leetcode/longest_common_prefix.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.size() == 0) {
-            return "";
-        }
-        return recursiveLCP(strs, 0, strs.size() - 1);
-    }
-    
-    // 0, 5
-    // 0, 2 and 3, 5
-    // 0, 1 and 
-    
-    string recursiveLCP(vector<string>& strs, int i, int j) {
-        if (i == j) {
-            return strs[i];
-        }
-        else if (j - i == 1) {
-            return getLCP(strs[i], strs[j]);
-        } else {
-            int mid = (i + j) / 2;
-            return getLCP(recursiveLCP(strs, i, mid), recursiveLCP(strs, mid + 1, j));
-        }
-    }
-    
-    string getLCP(string s1, string s2) {
-            string result = "";
-            for(int i = 0; (i < s1.length() && i < s2.length()); i++) {
-                if (s1[i] == s2[i]) {
-                    result += s1[i];
-                } else {
-                    break;
+        if(strs.size() == 0) return "";
+
+        string lcp = strs[0];
+
+        for(string s : strs) {
+            if(lcp == s) continue;
+            
+            for(string s : strs) {
+                if(lcp == s) continue;
+
+                int i=0;
+                for(; (i<lcp.size() && i<s.size()); i++) {
+                    if(lcp[i] != s[i]) {
+                        break;
+                    }
                 }
+                lcp = lcp.substr(0, i);
+
+                if(lcp == "") break;
             }
-            return result;
+            
+            if(lcp == "") break;
         }
-    
-    
+
+        return lcp;
+    }
 };
