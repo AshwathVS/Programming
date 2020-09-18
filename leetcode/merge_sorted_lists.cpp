0a7b937f83a6ea36d1f7a2c001a3ceb3953e14b7
Ashwath V S <ashsat.vijayan122@gmail.com>
2020-09-19 00:53:19 +0100

    Update merge_sorted_lists.cpp

--- leetcode/merge_sorted_lists.cpp
+++ leetcode/merge_sorted_lists.cpp
@@ -9,58 +9,34 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* result = NULL;
-        ListNode* head = NULL;
+        ListNode* head=NULL, *iter=NULL;
         
-        while (true) {
-            
-            if (l1 == NULL && l2 == NULL) {
-                break;
-            }
-            else if (l1 == NULL) {
-                while(l2 != NULL) {
-                    int val = l2->val;
-                    if (result == NULL) {
-                        result = new ListNode(val);
-                        head = result;
-                    } else {
-                        result->next = new ListNode(val);
-                        result = result->next;
-                    }
-                    l2 = l2->next;
-                }
-            } else if (l2 == NULL) {
-                while(l1 != NULL) {
-                    int val = l1->val;
-                    if (result == NULL) {
-                        result = new ListNode(val);
-                        head = result;
-                    } else {
-                        result->next = new ListNode(val);
-                        result = result->next;
-                    }
-                    l1 = l1->next;
-                }
+        while(l1 != NULL || l2 != NULL) {
+            int val;
+            if(l1 == NULL) {
+                val = l2->val;
+                l2 = l2->next;
+            } else if(l2 == NULL) {
+                val = l1->val;
+                l1 = l1->next;
             } else {
-                int val;
-                if (l1->val < l2->val) {
+                if(l1->val <= l2->val) {
                     val = l1->val;
                     l1 = l1->next;
                 } else {
                     val = l2->val;
                     l2 = l2->next;
                 }
-                
-                if (result == NULL) {
-                    result = new ListNode(val);
-                    head = result;
-                } else {
-                    result->next = new ListNode(val);
-                    result = result->next;
-                }
+            }
+            
+            if(iter == NULL) {
+                iter = new ListNode(val);
+                head = iter;
+            } else {
+                iter->next = new ListNode(val);
+                iter = iter->next;
             }
         }
-        
         
         return head;
     }
