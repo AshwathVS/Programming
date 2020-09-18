c1d141248b34310514213cae856ea386cc9f6cd9
Ashwath V S <ashsat.vijayan122@gmail.com>
2020-09-18 18:02:05 +0100

    Update longest_common_prefix.cpp

--- leetcode/longest_common_prefix.cpp
+++ leetcode/longest_common_prefix.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string> strs) {
         if(strs.size() == 0) return "";
 
         string lcp = strs[0];
 
         for(string s : strs) {
             if(lcp == s) continue;
-            
-            for(string s : strs) {
-                if(lcp == s) continue;
 
-                int i=0;
-                for(; (i<lcp.size() && i<s.size()); i++) {
-                    if(lcp[i] != s[i]) {
-                        break;
-                    }
+            int i=0;
+            for(; (i<lcp.size() && i<s.size()); i++) {
+                if(lcp[i] != s[i]) {
+                    break;
                 }
-                lcp = lcp.substr(0, i);
-
-                if(lcp == "") break;
             }
-            
+            lcp = lcp.substr(0, i);
+
             if(lcp == "") break;
         }
 
